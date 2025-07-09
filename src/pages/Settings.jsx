d5ab6eb60d9cef2f6e0ea08b0bc3aca1fd8f2d66
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-07-09 11:19:33 +0200

    Updated Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -36,33 +36,54 @@ const Settings = () => {
       <Box
         bgcolor={'background.default'}
         color={'text.primary'}
-        sx={{ m: 5, typography: 'body1' }}
+        sx={{ m: { md: 5 }, mt: { xs: 5 }, typography: 'body1' }}
       >
         <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="">
+          <Box
+            sx={{
+              borderBottom: 1,
+              borderColor: 'divider',
+            }}
+          >
+            <TabList onChange={handleChange} aria-label="settings tabs">
               <Tab
                 icon={<PersonOutlineOutlinedIcon />}
                 iconPosition="start"
-                label="Profile"
+                label={
+                  <Box sx={{ display: { xs: 'none', sm: 'inline' } }}>
+                    Profile
+                  </Box>
+                }
                 value="1"
               />
               <Tab
                 icon={<NotificationsNoneOutlinedIcon />}
                 iconPosition="start"
-                label="Notifications"
+                label={
+                  <Box sx={{ display: { xs: 'none', sm: 'inline' } }}>
+                    Notifications
+                  </Box>
+                }
                 value="2"
               />
               <Tab
                 icon={<ShieldOutlinedIcon />}
                 iconPosition="start"
-                label="Security"
+                label={
+                  <Box sx={{ display: { xs: 'none', sm: 'inline' } }}>
+                    Security
+                  </Box>
+                }
                 value="3"
               />
               <Tab
                 icon={<RemoveRedEyeOutlinedIcon />}
                 iconPosition="start"
-                label="Appearance"
+                label={
+                  <Box sx={{ display: { xs: 'none', sm: 'inline' } }}>
+                    Appearance
+                  </Box>
+                }
                 value="4"
               />
             </TabList>
@@ -75,7 +96,16 @@ const Settings = () => {
 
             <Box
               component="form"
-              sx={{ '& .MuiTextField-root': { width: '45%' }, mt: 5, ml: 5 }}
+              sx={{
+                display: 'flex',
+                flexDirection: { xs: 'column', md: 'row' },
+                '& .MuiTextField-root': {
+                  width: { md: '45%', xs: '80%' },
+                  mb: { xs: 2, md: 0 },
+                },
+                mt: 5,
+                ml: { md: 5 },
+              }}
               noValidate
               autoComplete="off"
             >
@@ -84,22 +114,22 @@ const Settings = () => {
                 id="outlined-required"
                 label="First Name"
                 defaultValue="John"
-                sx={{ mr: 1 }}
+                sx={{ mr: { md: 1 } }}
               />
               <TextField
                 type="text"
                 id="outlined-required"
                 label="Last Name"
                 defaultValue="Doe"
-                sx={{ ml: 3 }}
+                sx={{ ml: { md: 3 } }}
               />
             </Box>
 
             <FormControl
               sx={{
-                mt: 3,
-                ml: 5,
-                width: '90%',
+                mt: { md: 3 },
+                ml: { md: 5 },
+                width: { md: '90%', xs: '80%' },
               }}
               noValidate
               autoComplete="off"
@@ -114,9 +144,9 @@ const Settings = () => {
 
             <FormControl
               sx={{
-                mt: 3,
-                ml: 5,
-                width: '90%',
+                mt: { md: 3, xs: 2 },
+                ml: { md: 5 },
+                width: { md: '90%', xs: '80%' },
               }}
               noValidate
               autoComplete="off"
@@ -131,9 +161,9 @@ const Settings = () => {
 
             <FormControl
               sx={{
-                mt: 3,
-                ml: 5,
-                width: '90%',
+                mt: { md: 3, xs: 2 },
+                ml: { md: 5 },
+                width: { md: '90%', xs: '80%' },
               }}
               noValidate
               autoComplete="off"
@@ -147,7 +177,7 @@ const Settings = () => {
               />
             </FormControl>
 
-            <Box sx={{ textTransform: 'uppercase', mt: 3, ml: 4 }}>
+            <Box sx={{ textTransform: 'uppercase', mt: 3, ml: { md: 4 } }}>
               <Button variant="contained" sx={{ m: 1 }}>
                 Save changes
               </Button>
@@ -156,7 +186,15 @@ const Settings = () => {
               </Button>
             </Box>
           </TabPanel>
-          <TabPanel value="2">ITem two</TabPanel>
+
+          <TabPanel value="2">
+            <Typography variant="h6" sx={{ fontWeight: 'normal', mb: 3 }}>
+              Notification Preferences
+            </Typography>
+            <Typography variant="p" sx={{}}>
+              Email Notifications
+            </Typography>
+          </TabPanel>
           <TabPanel value="3">ITem three</TabPanel>
           <TabPanel value="4">ITem four</TabPanel>
         </TabContext>
